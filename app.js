d3e76f25b5f9bf7c803b5b4d1e35da5a8fb9d258
rahul Sharma <sharmarahul1729@gmail.com>
2021-06-15 20:52:18 +0530

    Last Commit maybe

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var bodyparser = require('body-parser');
-
-// mongoose.connect('mongodb://localhost/covid19', {useNewUrlParser: true,useUnifiedTopology: true}).then(() => {
-//     console.log("Connected to Database");
-//     }).catch((err) => {
-//         console.log("Not Connected to Database ERROR! ", err);
-// });
-
+var bodyparser = require("body-parser");
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.urlencoded({ extended: true }));
 
-var pageRoutes =  require('./routes/index');
-var worldcovid = require('./routes/worldcovid');
-var indiacovid = require('./routes/indiacovid');
-
-
+var pageRoutes = require("./routes/index");
+var worldcovid = require("./routes/worldcovid");
+var indiacovid = require("./routes/indiacovid");
 
 app.use(pageRoutes);
 app.use(worldcovid);
 app.use(indiacovid);
-
 
 var PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server is running on " + PORT));
